4f5bc60d38d6a1ad4e1bd1b7cadae048cdfe7701
Joakim <VilloKodeHode@gmail.com>
2023-01-02 13:18:02 +0100

    1

--- src/routes/Contact/ContactPage.jsx
+++ src/routes/Contact/ContactPage.jsx
@@ -1,10 +1,14 @@
+import NavBar from "../../component/NavBar/NavBar";
 import FooterSection from "../../component/Portfolio/FooterSection/FooterSection";
 
 function ContactPage() {
   return (
-    <div className="bg-purple-900 rounded-xl p-4 m-20">
-      <FooterSection />
-    </div>
+    <>
+      <NavBar />
+      <div className="bg-purple-900 rounded-xl p-4 m-20">
+        <FooterSection />
+      </div>
+    </>
   );
 }
 
