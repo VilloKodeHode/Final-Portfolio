2bf37f80b6e841f55c3f1834777d2ae1e9f9e420
Joakim <VilloKodeHode@gmail.com>
2023-01-13 14:37:48 +0100

    added animations to projectPage

---
+++ src/component/Particles/Polygon.jsx
@@ -0,0 +1,104 @@
+import React, { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { polygonPathName, loadPolygonPath } from "tsparticles-path-polygon";
+import "./polygon.css";
+
+function RenderHexagons() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadPolygonPath(engine);
+  }, []);
+
+  return (
+    <Particles
+      options={{
+        fpsLimit: 60,
+        particles: {
+          color: {
+            value: "#fbdb04",
+            animation: {
+              enable: true,
+              speed: 10,
+            },
+          },
+          move: {
+            attract: {
+              enable: true,
+              rotate: {
+                distance: 100,
+                x: 2000,
+                y: 2000,
+              },
+            },
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "destroy",
+            },
+            path: {
+              clamp: false,
+              enable: true,
+              delay: {
+                value: 0,
+              },
+              generator: polygonPathName,
+              options: {
+                sides: 6,
+                turnSteps: 30,
+                angle: 30,
+              },
+            },
+            random: false,
+            speed: 3,
+            straight: true,
+            trail: {
+              fillColor: "#000",
+              length: 20,
+              enable: true,
+            },
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 0,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: 2,
+          },
+        },
+        background: {
+          color: "#3c5570",
+        },
+        fullScreen: {
+          zIndex: -1,
+        },
+        detectRetina: true,
+        emitters: {
+          direction: "none",
+          rate: {
+            quantity: 1,
+            delay: 0.25,
+          },
+          size: {
+            width: 0,
+            height: 0,
+          },
+          position: {
+            x: 50,
+            y: 50,
+          },
+        },
+      }}
+      init={particlesInit}
+    />
+  );
+}
+
+export default RenderHexagons;
--- src/routes/Contact/ContactPage.jsx
+++ src/routes/Contact/ContactPage.jsx
@@ -1,10 +1,12 @@
 import HamburgerBar from "../../component/NavBar/HamburgerBar";
 import NavBar from "../../component/NavBar/NavBar";
+import RenderHexagons from "../../component/Particles/Polygon";
 import ContactSection from "../../component/Portfolio/ContactSection/ContactSection";
 
 function ContactPage() {
   return (
     <>
+      <RenderHexagons />
       <ContactSection />
     </>
   );
