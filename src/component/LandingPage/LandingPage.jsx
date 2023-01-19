4d678be442cfa7d0a1ebbcb2796f25241a04eee3
Joakim Villo <villokodehode@gmail.com>
2023-01-19 23:20:31 +0100

    fixed landingPage again

--- src/component/LandingPage/LandingPage.jsx
+++ src/component/LandingPage/LandingPage.jsx
@@ -16,15 +16,48 @@ function LandingPage() {
       {/* <RenderHexagons /> */}
 
       <div className="absolute top-0 bottom-0 left-0 right-0 h-screen LandingPageBG"></div>
+
+      <div className="absolute top-1/2 -translate-x-1/2  bg-opacity-50">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn bg-fairy-400 text-water-100">
+            Tour my site
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-normal dropdown-content text-water-200 mt-0 p-2 shadow -translate-x-6 rounded-box w-48 backdrop-blur-sm z-50"
+          >
+            <li>
+              <Link className="m-auto text-3xl" to="/About">
+                Om
+              </Link>
+            </li>
+            <li>
+              <Link className="m-auto text-3xl" to="/Qualifications">
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link className="m-auto text-3xl" to="/Projects">
+                Prosjekter
+              </Link>
+            </li>
+            <li>
+              <Link className="m-auto text-3xl" to="/Contact">
+                Kontakt
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <div className=" top-0 left-0 flex flex-col w-full h-full z-50 font-Roboto tracking-widest select-none">
         <div className="landingPage z-50">
           <div className="lg:text-[250px] md:text-[150px] text-[100px] z-10 text-water-100 absolute left-1/2 -translate-x-1/2 top-36 h-fit">
             Villo
           </div>
-
           {/* Add a startscreen navigation in the center of the screen */}
 
-          <div className="landingPageSmaller leading-snug w-2/3 text-fairy-200 lg:text-[60px] md:text-[50px] text-[40px] absolute top-60 left-1/2 -translate-x-1/2 z-20">
+          <div className="landingPageSmaller leading-snug w-2/3 text-water-300 lg:text-[60px] md:text-[50px] text-[40px] absolute top-60 left-1/2 -translate-x-1/2 lg:-translate-y-2 md:-translate-y-8 sm:-translate-y-1/4 -translate-y-1/3 z-20">
             {/* <span className="menu menu-horizontal "> */}
             {/* <li>
                 <Link to="/Contact" className="p-0 text-fairy-100 stroked"> */}
